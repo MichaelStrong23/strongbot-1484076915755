d968a2c9d13b2f9e04590c0a43c9b3a706f6a9aa
MichaelStrong23 <michael.strong@strong.com.do>
2017-01-10 16:05:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
+ var watson = require('watson-developer-cloud');
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -26,3 +27,59 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+/*************************************************************************************************
+
+ Watson Conversation
+
+*************************************************************************************************/
+var conversation = watson.conversation({
+    url: 'https://gateway.watsonplatform.net/conversation/api',   
+    username: '98058639-8bcb-4494-8138-e8cef3b99bc1',   // Set to your conversation username
+    password: 'zo3vPFzRIAx1',   // Set to your conversation password
+    version_date: '2016-07-11',
+    version: 'v1'
+});
+
+// Allow clients to interact with the bot
+app.post('/api/bot', function(req, res) {
+    
+    console.log("Got request for Le Bot");
+    console.log("Request is: ",req);
+
+    var workspace = 'WORKSPACE_ID'; // Set to your Conversation workspace ID
+
+    if (!workspace) {
+        console.log("No workspace detected. Cannot run the Watson Conversation service.");
+    }
+
+    var params = {
+        workspace_id: workspace,
+        context: {}, // Null context indicates new conversation
+        input: {}    // Holder for message
+    };
+
+    // Update options to send to conversation service with the user input and a context if one exists
+    if (req.body) {
+        if (req.body.input) {
+            params.input = req.body.input;
+        }
+
+        if (req.body.context) {
+            params.context = req.body.context;
+        }
+    }
+
+    // Send message to the conversation service with the current context
+    conversation.message(params, function(err, data) {
+        if (err) {
+            console.log("Error in sending message: ", err);
+            return res.status(err.code || 500).json(err);
+        }
+
+        console.log("Response: ", data);
+
+        return res.json(data);
+    });
+
+}); // End app.post '/api/bot'
+
