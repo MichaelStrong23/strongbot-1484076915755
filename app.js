c91b1615ced0618e14842f387d95be7e8393c83a
MichaelStrong23 <michael.strong@strong.com.do>
2017-01-10 16:52:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var appEnv = cfenv.getAppEnv();
 
 var bodyParser = require('body-parser'),
 
-var watson = require('watson-developer-cloud');
+var watson = require('watson-develomer-cloud');
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
